e5137d75835fbcc02d731ea76334abb9009fbc5b
krishna bihari rajpt <krishnarajput@questglt.com>
2021-06-24 16:00:55 +0530

    secrets update

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,7 @@ const WETH = artifacts.require('WETH.sol');
 
 module.exports = async function (deployer, _network, addresses) {
   let weth;
-  const FACTORY_ADDRESS = '0xcbCB8632CcD3aDf2f85a5E62e200dF6c6EcbeF56';
+  const FACTORY_ADDRESS = '0x13EF9ACAedd656B4F7E4c1a7eb3CCbadB020BbbD';
 
   if(_network === 'mainnet') {
     weth = await WETH.at('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2');
